97bb2416945775ed7f36fed6578b5ae869c383cf
ReynaCansecoHernandez <l19350287@tuxtepec.tecnm.mx>
2023-03-02 10:01:21 -0600

    Inicializar el Proyecto - Calculadora

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+function App() {
+  return (
+    <div>
+      <h1>Calculadora</h1>
+      <table style={{width: '400px'}}>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+        <tr>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+          <td style={{border: '1px solid black'}}>0</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default App
